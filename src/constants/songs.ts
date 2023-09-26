66c17c34bf0427a09a0a1b4f1a667de65add723e
hglenn2k <hglenn2k@gmail.com>
2023-09-26 11:54:05 -0700

    added beyonce birthday

--- src/constants/songs.ts
+++ src/constants/songs.ts
@@ -54,4 +54,104 @@ export const songs = [
     name: "Single Ladies (Put a Ring on It)",
     youtubeId: "4m1EFMoRFvY",
   },
+  {
+    artist: "Beyoncé",
+    name: "Beautiful Liar",
+    youtubeId: "cLSHjCiB7Xs",
+  },
+  {
+    artist: "Beyoncé",
+    name: "Irreplacable",
+    youtubeId: "UtkGxormq2k",
+  },
+  {
+    artist: "Beyoncé",
+    name: "Green Light",
+    youtubeId: "WcHmra312rI",
+  },
+  {
+    artist: "Beyoncé",
+    name: "Kitty Kat",
+    youtubeId: "AoNH5YK_MJ4",
+  },
+  {
+    artist: "Beyoncé",
+    name: "Welcome to Hollywood",
+    youtubeId: "Qe8EPns4Mow",
+  },
+  {
+    artist: "Beyoncé",
+    name: "Upgrade U",
+    youtubeId: "UYQPR-5DK3g",
+  },
+  {
+    artist: "Beyoncé",
+    name: "Flaws and All",
+    youtubeId: "k5rY1X9rMCY",
+  },
+  {
+    artist: "Beyoncé",
+    name: "World Wide Woman",
+    youtubeId: "BESujieNc1Y",
+  },
+  {
+    artist: "Beyoncé",
+    name: "Get Me Bodied",
+    youtubeId: "kF7NUfCFyvk",
+  },
+  {
+    artist: "Beyoncé",
+    name: "If",
+    youtubeId: "1Xn-PUHV56w",
+  },
+  {
+    artist: "Beyoncé",
+    name: "Freakum Dress",
+    youtubeId: "0dqE6vnhFhE",
+  },
+  {
+    artist: "Beyoncé",
+    name: "Suga Mama",
+    youtubeId: "SA2R-Szr1vc",
+  },
+  {
+    artist: "Beyoncé",
+    name: "Deja Vu",
+    youtubeId: "TWo_j_RhyYE",
+  },
+  {
+    artist: "Beyoncé",
+    name: "Ring The Alarm",
+    youtubeId: "7izxEr-tj1k",
+  },
+  {
+    artist: "Beyoncé",
+    name: "Resentment",
+    youtubeId: "Tzp2vUp3kyo",
+  },
+  {
+    artist: "Beyoncé",
+    name: "Listen",
+    youtubeId: "honPlOzVRCU",
+  },
+  {
+    artist: "Beyoncé",
+    name: "",
+    youtubeId: "",
+  },
+  {
+    artist: "Beyoncé",
+    name: "",
+    youtubeId: "",
+  },
+  {
+    artist: "Beyoncé",
+    name: "",
+    youtubeId: "",
+  },
+  {
+    artist: "Beyoncé",
+    name: "",
+    youtubeId: "",
+  },
 ];
